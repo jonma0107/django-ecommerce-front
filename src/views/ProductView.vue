cb85b55b621b384026378726fa25a044b43c03c4
jonma0107 <jduplantier44@gmail.com>
2023-08-07 22:45:56 -0500

    [UPDATE] make it possible to add to cart

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -19,7 +19,7 @@
             <input type="number" class="input" min="1" v-model="quantity">
           </div>
           <div class="conrtol">
-            <a href="#" class="button is-dark">Add to cart</a>
+            <a href="#" class="button is-dark" @click="addToCart">Add to cart</a>
           </div>
 
         </div>
@@ -30,6 +30,8 @@
 
 <script>
   import axios from 'axios'
+  import { toast } from 'bulma-toast'
+
   export default {
     name: 'Product',
     data(){
@@ -53,6 +55,25 @@
           .catch(error => {
             console.log(error)
           })
+      },
+      addToCart(){
+        if (isNaN(this.quantity) || this.quantity < 1){
+          this.quantity = 1
+        }
+        const item = {
+          product: this.product,
+          quantity: this.quantity
+        }
+        this.$store.commit('addToCart', item)
+
+        toast({
+                message: 'The product was added to the cart',
+                type: 'is-success',
+                dismissible: true,
+                pauseOnHover: true,
+                duration: 2000,
+                position: 'bottom-right',
+        })
       }
     }
   }
